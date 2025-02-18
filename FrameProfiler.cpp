a1033c375dbe6bfbe41778afd09c87de410c669f
Philip Schuchardt <vpicaver@gmail.com>
2025-02-17 17:47:30 -0800

    Fixed windows linker warnings

--- FrameProfiler.cpp
+++ FrameProfiler.cpp
@@ -1,1 +1,33 @@
 #include "MarkScope/FrameProfiler.h"
+
+MarkScope::FrameProfiler::FrameProfiler(QQmlApplicationEngine *engine) :
+    QObject(engine)
+{
+#ifdef ITT_AVAILABLE
+    domain = __itt_domain_create(L"FrameProfiler");
+#endif
+#ifdef __APPLE__
+    log = os_log_create("com.markscope.frameprofiler", OS_LOG_CATEGORY_POINTS_OF_INTEREST);
+    signpost_id = os_signpost_id_generate(log);
+    Q_ASSERT(signpost_id != OS_SIGNPOST_ID_INVALID);
+    Q_ASSERT(signpost_id != OS_SIGNPOST_ID_NULL);
+#endif
+
+    QObject *rootObject = nullptr;
+    if (!engine->rootObjects().isEmpty()) {
+        rootObject = engine->rootObjects().first();
+    }
+
+    QQuickWindow *window = qobject_cast<QQuickWindow *>(rootObject);
+    if (window) {
+        connect(window, &QQuickWindow::frameSwapped, this, &FrameProfiler::profileFrame, Qt::DirectConnection);
+    } else {
+        qWarning("Failed to get QQuickWindow, frame profiling will not work");
+    }
+}
+
+MarkScope::FrameProfiler::~FrameProfiler() {
+#ifdef ITT_AVAILABLE
+    // __itt_domain_destroy(domain); // Uncomment if necessary
+#endif
+}
--- MarkScope/FrameProfiler.h
+++ MarkScope/FrameProfiler.h
@@ -25,37 +25,9 @@ class MARKSCOPE_EXPORT FrameProfiler : public QObject {
     Q_OBJECT
 
 public:
-    FrameProfiler(QQmlApplicationEngine* engine) :
-        QObject(engine)
-    {
-#ifdef ITT_AVAILABLE
-        domain = __itt_domain_create(L"FrameProfiler");
-#endif
-#ifdef __APPLE__
-        log = os_log_create("com.markscope.frameprofiler", OS_LOG_CATEGORY_POINTS_OF_INTEREST);
-        signpost_id = os_signpost_id_generate(log);
-        Q_ASSERT(signpost_id != OS_SIGNPOST_ID_INVALID);
-        Q_ASSERT(signpost_id != OS_SIGNPOST_ID_NULL);
-#endif
+    FrameProfiler(QQmlApplicationEngine* engine);
 
-        QObject *rootObject = nullptr;
-        if (!engine->rootObjects().isEmpty()) {
-            rootObject = engine->rootObjects().first();
-        }
-
-        QQuickWindow *window = qobject_cast<QQuickWindow *>(rootObject);
-        if (window) {
-            connect(window, &QQuickWindow::frameSwapped, this, &FrameProfiler::profileFrame, Qt::DirectConnection);
-        } else {
-            qWarning("Failed to get QQuickWindow, frame profiling will not work");
-        }
-    }
-
-    ~FrameProfiler() {
-#ifdef ITT_AVAILABLE
-        // __itt_domain_destroy(domain); // Uncomment if necessary
-#endif
-    }
+    ~FrameProfiler();
 
 private:
     bool firstFrame = true;
